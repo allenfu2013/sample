7af58ffe91f5e5afa7b4eb61a821020517f244a2
allenfu2013 <hope_fu@163.com>
2018-06-05 23:20:12 +0800

    command & adapter

---
+++ src/main/java/org/allen/sample/designpattern/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package org.allen.sample.designpattern.adapter;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
---
+++ src/main/java/org/allen/sample/designpattern/command/Order.java
@@ -0,0 +1,5 @@
+package org.allen.sample.designpattern.command;
+
+public interface Order {
+    void execute();
+}
