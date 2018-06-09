69e474334b09051e275d63b7de293ac2ffe16182
allenfu2013 <hope_fu@163.com>
2018-06-09 10:59:14 +0800

    responsibility chain

---
+++ src/main/java/org/allen/sample/designpattern/chainofresponsibility/example/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package org.allen.sample.designpattern.chainofresponsibility.example;
+
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
